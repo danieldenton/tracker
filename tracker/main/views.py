178061a9216ea73dc61d4fb624aaa861a311a4e0
danieldenton <danielmdenton@gmail.com>
2022-08-16 07:45:22 -0700

    base template

--- tracker/main/views.py
+++ tracker/main/views.py
@@ -3,14 +3,25 @@ from django.http import HttpResponse, HttpResponseRedirect
 from .models import  Application
 from .forms import AddApplication
 
-def index(response):
-    return render(response, "main/index.html", {})
+def home(response):
+    app = Application.objects.get()
+    context = {
+        "company": app.company,
+        "job_title": app.job_title,
+        "date_listed": app.date_listed,
+        "date_applied": app.date_applied,
+        "follow_up": app.follow_up,
+        "result": app.result,
+        "contacts": app.contacts,
+        "notes": app.notes
+    }
+    return render(response, "main/home.html", context)
 
 
-def create(response):
+def add(response):
     if response.method == "POST":
         form = AddApplication(response.POST)
         if form.is_valid():
             n = form.cleaned_data["name"]
            
-        return render(response)
+    return render(response, "main/add.html", {})
