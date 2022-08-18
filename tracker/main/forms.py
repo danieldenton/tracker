47349f40b4724c405d76c665182d5962122e8da4
danieldenton <danielmdenton@gmail.com>
2022-08-17 22:41:03 -0700

    fully functional form

--- tracker/main/forms.py
+++ tracker/main/forms.py
@@ -5,7 +5,7 @@ class AddApplication(forms.Form):
     job_title = forms.CharField(label="title", max_length=100)
     date_listed = forms.DateField(label="listed", required=False)
     date_applied = forms.DateField(label="applied", required=True)
-    follow_up = forms.BooleanField(label="follow-up")
+    follow_up = forms.BooleanField(label="follow-up", required=False)
     result = forms.CharField(label="result", max_length=20)
     contacts = forms.CharField(label="contacts", max_length=200)
     notes = forms.CharField(label="notes", max_length=300)
--- tracker/main/views.py
+++ tracker/main/views.py
@@ -8,12 +8,32 @@ def home(response):
     context = {
         'application': application
          } 
-       
- 
+
     return render(response, "main/home.html", context)
 
 
 def add(response):
-    form = AddApplication()
+    if response.method == "POST":
+        form = AddApplication(response.POST)
+
+        if form.is_valid():
+            com = form.cleaned_data['company']
+            jt = form.cleaned_data['job_title']
+            dl = form.cleaned_data['date_listed']
+            da = form.cleaned_data['date_applied']
+            fu = form.cleaned_data['follow_up']
+            r =form.cleaned_data['result']
+            con =form.cleaned_data['contacts']
+            n =form.cleaned_data['notes']
+            l = form.cleaned_data['link']
+            a = Application(company=com, job_title=jt, date_listed=dl, date_applied=da,follow_up=fu, result=r, contacts=con, notes=n, link=l)
+            a.save()
+
+        return render(response, "main/home.html")
+
+    else:
+        form = AddApplication()
+
+    
         
     return render(response, "main/add.html", {'form': form})
