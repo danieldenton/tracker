80876fe94ef52595faf33d07df9da2bba966d93c
danieldenton <danielmdenton@gmail.com>
2022-08-20 19:07:10 -0700

    updated forms for options

--- tracker/main/forms.py
+++ tracker/main/forms.py
@@ -1,15 +1,27 @@
-from django import forms
+from django.forms import Form, ModelForm, CharField, DateField
+from .models import Application
 
-class AddApplication(forms.Form):
-    company = forms.CharField(label="company", max_length=100, required=True)
-    job_title = forms.CharField(label="title", max_length=100, required=True)
-    date_listed = forms.DateField(label="listed", required=False)
-    date_applied = forms.DateField(label="applied", required=True)
-    result = forms.CharField(label="result", max_length=20, required=False)
-    contacts = forms.CharField(label="contacts", max_length=200, required=False)
-    notes = forms.CharField(label="notes", max_length=300, required=False)
-    link = forms.CharField(label="link", max_length=200, required=True)
 
-class EditApplication(forms.Form):
-    result = forms.CharField(label="result", max_length=20, required=False)
-    notes = forms.CharField(label="notes", max_length=300, required=False)
+class AddApplication(ModelForm):
+    class Meta:
+        model = Application
+        fields = '__all__'
+
+class EditApplication(ModelForm):
+    class Meta:
+        model = Application
+        fields = ['notes', 'result']
+
+# class AddApplication(Form):
+#     company = CharField(label="company", max_length=100, required=True)
+#     job_title = CharField(label="title", max_length=100, required=True)
+#     date_listed = DateField(label="listed", required=False)
+#     date_applied = DateField(label="applied", required=True)
+#     result = CharField(label="result", max_length=20, required=False)
+#     contacts = CharField(label="contacts", max_length=200, required=False)
+#     notes = CharField(label="notes", max_length=300, required=False)
+#     link = CharField(label="link", max_length=200, required=True)
+
+# class EditApplication(Form):
+#     result = CharField(label="result", max_length=20, required=False)
+#     notes = CharField(label="notes", max_length=300, required=False)
--- tracker/main/views.py
+++ tracker/main/views.py
@@ -1,5 +1,5 @@
 # from rest_framework import routers
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import  Application
 from .forms import AddApplication, EditApplication
@@ -18,18 +18,18 @@ def add(response):
         form = AddApplication(response.POST)
 
         if form.is_valid():
-            com = form.cleaned_data['company']
-            jt = form.cleaned_data['job_title']
-            dl = form.cleaned_data['date_listed']
-            da = form.cleaned_data['date_applied']
-            r =form.cleaned_data['result']
-            con =form.cleaned_data['contacts']
-            n =form.cleaned_data['notes']
-            l = form.cleaned_data['link']
-            a = Application(company=com, job_title=jt, date_listed=dl, date_applied=da, result=r, contacts=con, notes=n, link=l)
-            a.save()
+            # com = form.cleaned_data['company']
+            # jt = form.cleaned_data['job_title']
+            # dl = form.cleaned_data['date_listed']
+            # da = form.cleaned_data['date_applied']
+            # r =form.cleaned_data['result']
+            # con =form.cleaned_data['contacts']
+            # n =form.cleaned_data['notes']
+            # l = form.cleaned_data['link']
+            # a = Application(company=com, job_title=jt, date_listed=dl, date_applied=da, result=r, contacts=con, notes=n, link=l)
+            form.save()
 
-        return render(response, "main/add.html", {})
+        return redirect('/')
 
     else:
         form = AddApplication()
@@ -41,15 +41,15 @@ def add(response):
 def edit(response, id):
     application = Application.objects.get(id=id)
     if response.method == "PUT":
+        form = EditApplication(response.PUT, instance=application)
 
         if form.is_valid():
-            form = EditApplication(response.PUT)
-            r =form.cleaned_data['result']
-            n =form.cleaned_data['notes']
+            r =form.cleaned_data['result'] 
+            n =form.cleaned_data['notes']          
             a = Application(result=r, notes=n)
             a.objects.update()
 
-        return render(response, "main/edit.html", {})
+        return redirect('/')
 
     else:
         form = EditApplication()
